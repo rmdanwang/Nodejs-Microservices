d849d1167b609cde8fd976dca7d01949971b43af
rmdanwang <rm.danwang@gmail.com>
2019-11-09 15:39:32 -0600

    updated

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const express = require('express');
 var app = express();
 const PORT = 8080;
 const HOST = '0.0.0.0';
-app.get('/', (req, res) => {
+
+app.get('/', (req, res) => 
+{
   res.send('Docker and Nodejs');
 });
+
 app.listen(PORT, HOST);
 console.log(`Running on ${PORT}`);
