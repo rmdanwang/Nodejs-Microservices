73f91841697549c560fef2812a19e450c7baf77c
rmdanwang <rm.danwang@gmail.com>
2019-11-08 22:46:32 -0600

    initial update

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -d -p 49170:8080 rmdanwang/node-web-app
---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+const express = require('express');
+// Constants
+
+var app = express();
+const PORT = 8080;
+const HOST = '0.0.0.0';
+app.get('/', (req, res) => {
+  res.send('Docker and Nodejs');
+});
+app.listen(PORT, HOST);
+console.log(`Running on ${PORT}`);
